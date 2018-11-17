45f4b2232d3f1dccad3c57988329a334600955c8
ivan knapp <ivanknapp@yandex.ru>
2018-11-18 00:14:20 +0300

    fix script, add method for page redirect

--- src/main/java/ru/knappia/bars/repository/LegalEntityRepository.java
+++ src/main/java/ru/knappia/bars/repository/LegalEntityRepository.java
@@ -10,19 +10,19 @@ public interface LegalEntityRepository extends CrudRepository<LegalEntity, Integ
 
     List<LegalEntity> findAllByName(String name);
 
-    @Query(value = "select * from bar.bar " +
+    @Query(value = "select * from bar.entity " +
             "where to_tsvector('russian', name) @@ to_tsquery('russian', ?1)",
             nativeQuery = true
     )
     List<LegalEntity> searchByName(String name);
 
-    @Query(value = "select * from bar.bar " +
+    @Query(value = "select * from bar.entity " +
             "where to_tsvector('russian', address) @@ to_tsquery('russian', ?1)",
             nativeQuery = true
     )
     List<LegalEntity> searchByAddress(String address);
 
-    @Query(value = "select * from bar.bar " +
+    @Query(value = "select * from bar.entity " +
             "where to_tsvector('russian', district) @@ to_tsquery('russian', ?1)",
             nativeQuery = true
     )
--- src/main/java/ru/knappia/bars/service/BarService.java
+++ src/main/java/ru/knappia/bars/service/BarService.java
@@ -3,6 +3,7 @@ package ru.knappia.bars.service;
 import ru.knappia.bars.repository.LegalEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BarService {
     List<LegalEntity> getAllBars();
@@ -16,4 +17,6 @@ public interface BarService {
     List<LegalEntity> searchByAddress(String address);
 
     List<LegalEntity> searchByDistrict(String district);
+
+    Optional<LegalEntity> findBarById(Integer id);
 }
